93174b0e76f584e01bb78909d54a78949dffff01
Rodolfo Augusto <r.augusto1892@gmail.com>
2020-07-01 00:52:05 -0300

    Exlusao de usuário

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -7,12 +7,21 @@ module.exports = {
 
     const emailExistente = Usuarios.some(e => e.email === args.email)
 
-    if(emailExistente) throw new Error("E-mail já cadastrado!")
+    if (emailExistente) throw new Error("E-mail já cadastrado!")
 
     const novo = {
       id: proximoID(), ...args, perfil_id: 1, status: 'ATIVO'
     }
     Usuarios.push(novo)
     return novo
+  },
+
+  excluirUsuario(_, {id}) {
+    const index = Usuarios.findIndex(i => i.id === id)
+
+    if (index < 0) return null
+
+    const excluidos = Usuarios.splice(index, 1)
+    return excluidos
   }
 }
