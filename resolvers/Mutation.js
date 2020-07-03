02960cf550959394b4d747e7ac67b40addf440d8
Rodolfo Augusto <r.augusto1892@gmail.com>
2020-07-02 23:06:49 -0300

    Alteração de dados de um usuário

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -2,13 +2,9 @@ const {Usuarios, proximoID} = require('../data/db')
 
 module.exports = {
   //{nome, email, idade}
-
   novoUsuario(_, args) {
-
     const emailExistente = Usuarios.some(e => e.email === args.email)
-
     if (emailExistente) throw new Error("E-mail já cadastrado!")
-
     const novo = {
       id: proximoID(), ...args, perfil_id: 1, status: 'ATIVO'
     }
@@ -18,10 +14,32 @@ module.exports = {
 
   excluirUsuario(_, {id}) {
     const index = Usuarios.findIndex(i => i.id === id)
-
     if (index < 0) return null
-
     const excluidos = Usuarios.splice(index, 1)
     return excluidos ? excluidos[0] : null
+  },
+
+  alterarUsuario(_, args) {
+    const index = Usuarios.findIndex(i => i.id === args.id)
+    if (index < 0) return null
+
+    /*const alterado = {
+      ...Usuarios[index],
+      ...args
+    }
+     //Busca o indice informado, exclui um item a partir desse index e adiciona na posição exlcuida
+    Usuarios.splice(index, 1, alterado)
+    return alterado
+    */
+
+    args.nome ? Usuarios[index].nome = args.nome : Usuarios[index].nome
+    args.email ? Usuarios[index].email = args.email : Usuarios[index].email
+    args.idade ? Usuarios[index].idade = args.idade : Usuarios[index].idade
+
+    return Usuarios[index]
+
   }
 }
+
+
+
