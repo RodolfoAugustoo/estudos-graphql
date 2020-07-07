ac6af8f710da7627788d7a632d87ad193a5f0255
Rodolfo Augusto <r.augusto1892@gmail.com>
2020-07-07 01:00:28 -0300

    Trabalhando com input

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -2,11 +2,11 @@ const {Usuarios, proximoID} = require('../data/db')
 
 module.exports = {
   //{nome, email, idade}
-  novoUsuario(_, args) {
-    const emailExistente = Usuarios.some(e => e.email === args.email)
+  novoUsuario(_, {dados}) {
+    const emailExistente = Usuarios.some(e => e.email === dados.email)
     if (emailExistente) throw new Error("E-mail já cadastrado!")
     const novo = {
-      id: proximoID(), ...args, perfil_id: 1, status: 'ATIVO'
+      id: proximoID(), ...dados, perfil_id: 1, status: 'ATIVO'
     }
     Usuarios.push(novo)
     return novo
